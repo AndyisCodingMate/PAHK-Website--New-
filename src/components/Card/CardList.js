8d73f756bc1d436568211e292e4c4979b12e7d09
Andy Poon <andypoon923@gemtrifi.com>
2023-09-29 09:19:50 -0700

    Uploaded copy of repository from old GitHub Account..
    Uploaded Copy of repository from old Github account.

---
+++ src/components/Card/CardList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+
+
+function CardList({items}){
+    return(
+        <div >
+            <Card/>   
+        </div>
+        )
+
+
+}
+export default CardList;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return(
+        <div className='footer'>
+            <div>
+                Copyright ©2020 Pacific Agriscience Hong Kong Ltd. All rights reserved.
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
